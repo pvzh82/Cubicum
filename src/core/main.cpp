30b3dfb4fdb9b51184197cececa18b3f7b82515b
pvzh82 <lluisgironara@gmail.com>
2025-02-15 08:56:36 +0100

    feat(camera): implement camera movement and fov changing with mouse wheel

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -17,13 +17,22 @@
 // define  funcs
 int width = 1920, height = 1080;
 
-void processInput(GLFWwindow *window);
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
 
 // comera
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool firstMouse = true;
+float yaw = -90.0f;
+float pitch = 0.0f;
+float lastX = width / 2.0;
+float lastY = height / 2.0;
+float fov = 45.0f;
 
 // time
 float deltaTime = 0.0f;
@@ -38,85 +47,83 @@ int main() {
 
   // create window and if statement
   GLFWwindow *window = glfwCreateWindow(width, height, ":)", NULL, NULL);
-  if (window == NULL)
-
-  {
+  if (window == NULL) {
     std::cout << "Failed to create window :c" << std::endl;
     glfwTerminate();
     return -1;
   }
-  glfwMakeContextCurrent(window);
-
   // initialize glad
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  glfwSetCursorPosCallback(window, mouse_callback);
+  glfwSetScrollCallback(window, scroll_callback);
+
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cout << "Failed to initialize GLAD :c" << std::endl;
     return -1;
   }
-  // size of rendering window
-  glViewport(0, 0, width, height);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-  glClearColor(0.455f, 0.701f, 1.0f, 0.8f);
-
   // triangle test
-  float vertices[] = {
+   float vertices[] = {
 
-    // pos                   //texture
-    // Front face
-    0.0f, 0.0f, 0.0f,        0.0f, 0.0f,
-    1.0f, 0.0f, 0.0f,        1.0f, 0.0f,
-    1.0f, 1.0f, 0.0f,        1.0f, 1.0f,
-    0.0f, 1.0f, 0.0f,        0.0f, 1.0f,
+     // pos                   //texture
+     // Front face
+     0.0f, 0.0f, 0.0f,        0.0f, 0.0f,
+     1.0f, 0.0f, 0.0f,        1.0f, 0.0f,
+     1.0f, 1.0f, 0.0f,        1.0f, 1.0f,
+     0.0f, 1.0f, 0.0f,        0.0f, 1.0f,
 
-    // Back face
-    0.0f, 0.0f, 1.0f,        0.0f, 0.0f,
-    1.0f, 0.0f, 1.0f,        1.0f, 0.0f,
-    1.0f, 1.0f, 1.0f,        1.0f, 1.0f,
-    0.0f, 1.0f, 1.0f,        0.0f, 1.0f,
+     // Back face
+     0.0f, 0.0f, 1.0f,        0.0f, 0.0f,
+     1.0f, 0.0f, 1.0f,        1.0f, 0.0f,
+     1.0f, 1.0f, 1.0f,        1.0f, 1.0f,
+     0.0f, 1.0f, 1.0f,        0.0f, 1.0f,
 
-    // Left face
-    0.0f, 0.0f, 0.0f,        0.0f, 0.0f,
-    0.0f, 0.0f, 1.0f,        1.0f, 0.0f,
-    0.0f, 1.0f, 1.0f,        1.0f, 1.0f,
-    0.0f, 1.0f, 0.0f,        0.0f, 1.0f,
+     // Left face
+     0.0f, 0.0f, 0.0f,        0.0f, 0.0f,
+     0.0f, 0.0f, 1.0f,        1.0f, 0.0f,
+     0.0f, 1.0f, 1.0f,        1.0f, 1.0f,
+     0.0f, 1.0f, 0.0f,        0.0f, 1.0f,
 
-    // Right face
-    1.0f, 0.0f, 0.0f,        0.0f, 0.0f,
-    1.0f, 0.0f, 1.0f,        1.0f, 0.0f,
-    1.0f, 1.0f, 1.0f,        1.0f, 1.0f,
-    1.0f, 1.0f, 0.0f,        0.0f, 1.0f,
+     // Right face
+     1.0f, 0.0f, 0.0f,        0.0f, 0.0f,
+     1.0f, 0.0f, 1.0f,        1.0f, 0.0f,
+     1.0f, 1.0f, 1.0f,        1.0f, 1.0f,
+     1.0f, 1.0f, 0.0f,        0.0f, 1.0f,
 
-    // Bottom face
-    0.0f, 0.0f, 0.0f,        0.0f, 0.0f,
-    1.0f, 0.0f, 0.0f,        1.0f, 0.0f,
-    1.0f, 0.0f, 1.0f,        1.0f, 1.0f,
-    0.0f, 0.0f, 1.0f,        0.0f, 1.0f,
+     // Bottom face
+     0.0f, 0.0f, 0.0f,        0.0f, 0.0f,
+     1.0f, 0.0f, 0.0f,        1.0f, 0.0f,
+     1.0f, 0.0f, 1.0f,        1.0f, 1.0f,
+     0.0f, 0.0f, 1.0f,        0.0f, 1.0f,
 
-    // Top face
-    0.0f, 1.0f, 0.0f,        0.0f, 0.0f,
-    1.0f, 1.0f, 0.0f,        1.0f, 0.0f,
-    1.0f, 1.0f, 1.0f,        1.0f, 1.0f,
-    0.0f, 1.0f, 1.0f,        0.0f, 1.0f,
+     // Top face
+     0.0f, 1.0f, 0.0f,        0.0f, 0.0f,
+     1.0f, 1.0f, 0.0f,        1.0f, 0.0f,
+     1.0f, 1.0f, 1.0f,        1.0f, 1.0f,
+     0.0f, 1.0f, 1.0f,        0.0f, 1.0f,
 
-    };
-    unsigned int indices[] = {
-        // Front face
-        0, 1, 2, 2, 3, 0,
-        
-        // Back face
-        4, 5, 6, 6, 7, 4,
-        
-        // Left face
-        8, 9, 10, 10, 11, 8,
-        
-        // Right face
-        12, 13, 14, 14, 15, 12,
-        
-        // Bottom face
-        16, 17, 18, 18, 19, 16,
-        
-        // Top face
-        20, 21, 22, 22, 23, 20
-    };
+     };
+     unsigned int indices[] = {
+         // Front face
+         0, 1, 2, 2, 3, 0,
+
+         // Back face
+         4, 5, 6, 6, 7, 4,
+
+         // Left face
+         8, 9, 10, 10, 11, 8,
+
+         // Right face
+         12, 13, 14, 14, 15, 12,
+
+         // Bottom face
+         16, 17, 18, 18, 19, 16,
+
+         // Top face
+         20, 21, 22, 22, 23, 20
+     };
 
   // shader getpath & compile
   // rootPath = "../";
@@ -180,10 +187,6 @@ int main() {
     std::cout << "couldnt load texture :c" << std::endl;
   }
 
-  glm::mat4 projection = glm::perspective(
-      glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
-  myShader.setMat4("projection", projection);
-
   stbi_image_free(data);
 
   glEnable(GL_DEPTH_TEST);
@@ -203,6 +206,11 @@ int main() {
     // activate shader
     myShader.useShader();
 
+    // projection
+    glm::mat4 projection = glm::perspective(
+        glm::radians(fov), (float)width / (float)height, 0.1f, 100.0f);
+    myShader.setMat4("projection", projection);
+
     // camera/view tranformation
     glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
     myShader.setMat4("view", view);
@@ -210,7 +218,8 @@ int main() {
     // render boxes
     glBindVertexArray(VAO);
     glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model, glm::vec3(0.0f, 0.0f, -5.0f)); // Move block in front of camera
+    model = glm::translate(
+        model, glm::vec3(0.0f, 0.0f, -5.0f)); // Move block in front of camera
     myShader.setMat4("model", model);
 
     glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
@@ -252,4 +261,48 @@ void processInput(GLFWwindow *window) {
 // window func
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
+}
+void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
+  if (firstMouse) {
+    lastX = xpos;
+    lastY = ypos;
+    firstMouse = false;
+  }
+
+  float xoffset = xpos - lastX;
+  float yoffset = lastY - ypos;
+  lastX = xpos;
+  lastY = ypos;
+
+  const float sensibility = 0.1f;
+  xoffset *= sensibility;
+  yoffset *= sensibility;
+
+  yaw += xoffset;
+  pitch += yoffset;
+
+  if (pitch > 89.0f) {
+    pitch = 89.0f;
+  }
+
+  if (pitch < -89.0f) {
+    pitch = -89.0f;
+  }
+
+  glm::vec3 direction;
+  direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+  direction.y = sin(glm::radians(pitch));
+  direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+  cameraFront = glm::normalize(direction);
+}
+
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
+
+  fov -= (float)yoffset;
+  if (fov < 1.0f) {
+    fov = 1.0f;
+  }
+  if (fov > 90.0f) {
+    fov = 90.0f;
+  }
 }
